77bb7868b8b3b66e13d31349dc97d8a2681d424c
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-10 12:06:17 -0700

    Add files via upload

---
+++ set,tuple,list,dictionary.py
@@ -0,0 +1,41 @@
+#------------------------------------------------------------------------
+#                   set,tuple,list,dictionary
+#------------------------------------------------------------------------
+
+'''    Python Tutorial for Beginners 4: Lists, Tuples, and Sets       '''
+
+import csv
+
+with open ('csvtext.csv', 'r') as new_file:
+    words = new_folder.reader(newfile)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
